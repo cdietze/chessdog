e51e5bbb33d2c70c3da46eea8c65fcf9a376acf0
christoph <none@none>
2010-05-05 22:14:52 +0200

    removed test for issue 19

--- src/test/java/com/christophdietze/jack/test/server/PlayGameTest.java
+++ src/test/java/com/christophdietze/jack/test/server/PlayGameTest.java
@@ -4,20 +4,9 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import com.christophdietze.jack.client.presenter.ApplicationContext;
-import com.christophdietze.jack.client.presenter.MatchInfo;
-import com.christophdietze.jack.client.presenter.MatchMode;
-import com.christophdietze.jack.common.JackService;
-import com.christophdietze.jack.common.JackServiceAsync;
-import com.christophdietze.jack.common.MatchStartedRemoteEvent;
-import com.christophdietze.jack.common.board.ChessUtils;
-import com.christophdietze.jack.common.board.Game;
-import com.christophdietze.jack.common.board.Move;
 import com.christophdietze.jack.server.JackServiceImpl;
-import com.christophdietze.jack.test.AsyncProxyGenerator;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -50,34 +39,4 @@ public class PlayGameTest extends TestCase {
 		chessService.makeMove(login1, "e7e5");
 	}
 
-	@Test
-	public void testIssue19() {
-		Injector injector = Guice.createInjector(new AbstractModule() {
-			@Override
-			protected void configure() {
-				Injector subInjector = Guice.createInjector();
-				JackService impl = subInjector.getInstance(JackServiceImpl.class);
-				JackServiceAsync mock = AsyncProxyGenerator.newProxy(JackServiceAsync.class, impl);
-				bind(JackServiceAsync.class).toInstance(mock);
-			}
-		});
-		JackServiceImpl chessService = injector.getInstance(JackServiceImpl.class);
-		long location1 = chessService.login();
-		long location2 = chessService.login();
-		chessService.postSeek(location1);
-		chessService.postSeek(location2);
-
-		assertEquals(MatchStartedRemoteEvent.class, chessService.poll(location1).getClass());
-		assertEquals(MatchStartedRemoteEvent.class, chessService.poll(location2).getClass());
-
-		chessService.abortMatch(location1);
-		ApplicationContext applicationContext = injector.getInstance(ApplicationContext.class);
-		applicationContext.setLocationId(location2);
-		MatchMode matchMode = injector.getInstance(MatchMode.class);
-		matchMode.activate(new MatchInfo(location1, location2, true));
-		Move m = ChessUtils.toMoveFromAlgebraic("e2e4");
-		matchMode.makeMove(m.getFrom(), m.getTo());
-		Game game = injector.getInstance(Game.class);
-		assertEquals(0, game.getPosition().getPly()); // no move was made because the match is already aborted
-	}
 }
