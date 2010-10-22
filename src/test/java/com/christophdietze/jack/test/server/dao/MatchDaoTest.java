4b9d3e747969b8aebeab4b399bafba45fe24b269
christoph <none@none>
2010-10-22 22:29:42 +0200

    case 21: When a match starts, all open challenges for that player should be cancelled

--- src/test/java/com/christophdietze/jack/test/server/dao/MatchDaoTest.java
+++ src/test/java/com/christophdietze/jack/test/server/dao/MatchDaoTest.java
@@ -5,10 +5,7 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import com.christophdietze.jack.server.dao.Location;
-import com.christophdietze.jack.server.dao.Match;
 import com.christophdietze.jack.server.dao.MatchDao;
-import com.christophdietze.jack.server.dao.NoActiveMatchForPlayerException;
-import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.inject.Guice;
@@ -42,26 +39,5 @@ public class MatchDaoTest extends TestCase {
 		assertEquals(1, matchDao.getAllMatches().size());
 		matchDao.createMatch(player3, player4);
 		assertEquals(2, matchDao.getAllMatches().size());
-	}
-
-	@Test
-	public void testHasPlayerActiveMatches() throws NoActiveMatchForPlayerException, EntityNotFoundException {
-		Key<Location> player1 = Location.newKey(11);
-		Key<Location> player2 = Location.newKey(12);
-		Key<Location> player3 = Location.newKey(13);
-		Key<Location> player4 = Location.newKey(14);
-
-		Injector injector = Guice.createInjector();
-		MatchDao matchDao = injector.getInstance(MatchDao.class);
-		assertFalse(matchDao.doesLocationHaveActiveMatch(player1));
-		matchDao.createMatch(player2, player3);
-		assertFalse(matchDao.doesLocationHaveActiveMatch(player1));
-		Match match = matchDao.createMatch(player1, player4);
-		assertTrue(match.isActive());
-		assertTrue(matchDao.doesLocationHaveActiveMatch(player1));
-		matchDao.makeLocationsMatchInactive(player1);
-		match = ObjectifyTestHelper.ofy().get(new Key<Match>(Match.class, match.getId()));
-		assertFalse(match.isActive());
-		assertFalse(matchDao.doesLocationHaveActiveMatch(player1));
 	}
 }
