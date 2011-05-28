57909253dbdbf064c91d5d4aa3d90f7ca5b592bc
christoph <none@none>
2011-05-28 13:55:48 +0200

    switch to sbt
    --HG--
    branch : switch_to_sbt

---
+++ project/build/SbtGwtGaeTestProject.scala
@@ -0,0 +1,26 @@
+import sbt._
+import sbt.Process._
+
+class SbtGwtGaeTestProject(info: ProjectInfo) extends AppengineProject(info) {
+	
+	val gwtUser = "com.google.gwt" % "gwt-user" % "2.3.0" % "provided"
+	val gwtDev = "com.google.gwt" % "gwt-dev" % "2.3.0" % "provided"
+	val validationApi = "javax.validation" % "validation-api" % "1.0.0.GA" % "provided" withSources()
+	val gwtServlet = "com.google.gwt" % "gwt-servlet" % "2.3.0"
+	val gwtModules = List("Jack", "JackAdmin", "JackEmbed").map("com.christophdietze.jack."+_)
+//	val gwtModules = List("Jack").map("com.christophdietze.jack."+_)
+
+	lazy val hi = task { println("hello World!"); None }
+	
+	lazy val gwtCompile = gwtCompileAction
+	def gwtCompileAction = task {
+		val command = "java -cp " + (compileClasspath +++ mainJavaSourcePath).absString + " com.google.gwt.dev.Compiler -war " + temporaryWarPath.absString + " " + gwtModules.mkString(" ")
+		command ! ; 
+		None
+	}
+	
+	override def prepareWebappAction = gwtCompileAction dependsOn(super.prepareWebappAction)
+	
+	override def compileClasspath = super.compileClasspath +++ descendents( info.projectPath / "lib_compile", "*.jar")
+
+}
