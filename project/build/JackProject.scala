192aebe378270f013e968a7c45d62438d1fadc12
christoph.dietze@gmail.com <christoph.dietze@gmail.com>
2011-06-15 00:34:56 +0200

    fixed resource loading of chessdog.embed.js
    --HG--
    branch : switch_to_sbt
    rename : src/main/java/com/christophdietze/jack/server/chessdog.embed.js => src/main/resources/chessdog.embed.js
    rename : src/main/java/com/christophdietze/jack/server/chessdog.embed.src.js => src/main/resources/chessdog.embed.src.js

--- project/build/JackProject.scala
+++ project/build/JackProject.scala
@@ -7,8 +7,8 @@ class JackProject(info: ProjectInfo) extends AppengineProject(info) {
 	val gwtDev = "com.google.gwt" % "gwt-dev" % "2.3.0" % "provided"
 	val validationApi = "javax.validation" % "validation-api" % "1.0.0.GA" % "provided" withSources()
 	val gwtServlet = "com.google.gwt" % "gwt-servlet" % "2.3.0"
-	val gwtModules = List("Jack", "JackAdmin", "JackEmbed").map("com.christophdietze.jack."+_)
-//	val gwtModules = List("Jack").map("com.christophdietze.jack."+_)
+//	val gwtModules = List("Jack", "JackAdmin", "JackEmbed").map("com.christophdietze.jack."+_)
+	val gwtModules = List("JackEmbed").map("com.christophdietze.jack."+_)
 
 	lazy val gwtCompile = gwtCompileAction
 	def gwtCompileAction = task {
---
+++ src/main/resources/chessdog.embed.src.js
@@ -0,0 +1,115 @@
+// This file should be compiled using the Google closure compiler
+// http://closure-compiler.appspot.com/home
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// @output_file_name chessdog.embed.js
+// ==/ClosureCompiler==
+chessdog = {}
+
+/**
+ * @constructor
+ */
+window.chessdog.Board = function(element) {
+	element.innerHTML = "<iframe width='600' height='385' frameborder='0' style='border: none;'></iframe>";
+	this.frame = element.childNodes[0];
+	this.wnd = this.frame.contentWindow;
+	this.doc = this.wnd.document;
+	var wnd = this.wnd;
+	var doc = this.doc;
+	doc.open();
+	doc
+			.write("<!doctype html>\n<html><head></head><body>"
+					+ "<div id='removeme' style='font-family: Arial Unicode MS,Arial,sans-serif;font-size: small;'>Loading chess board...</div>"
+					+ "</body></html>\n");
+	doc.close();
+
+	setTimeout(
+			function() {
+				var _gaq = _gaq || [];
+				_gaq.push([ '_setAccount', 'UA-5412976-12' ]);
+				_gaq.push([ '_setDomainName', 'none' ]);
+				_gaq.push([ '_setAllowLinker', true ]);
+				_gaq.push([ '_trackPageview' ]);
+				// since we're loading google analytics in the frame the queue
+				// must also be defined in the frame
+				wnd._gaq = _gaq;
+				(function() {
+					var ga = doc.createElement('script');
+					ga.type = 'text/javascript';
+					ga.async = true;
+					ga.src = ('https:' == doc.location.protocol ? 'https://ssl' : 'http://www')
+							+ '.google-analytics.com/ga.js';
+					doc.getElementsByTagName('head')[0].appendChild(ga);
+				})();
+
+				var scriptElement = doc.createElement("script");
+				scriptElement.type = "text/javascript";
+				scriptElement.src = "http://${Host}/jackembed/jackembed.nocache.js";
+				doc.getElementsByTagName('head')[0].appendChild(scriptElement);
+			}, 0);
+}
+
+/*
+ * At the time the JS engine gets here, the actual functions (e.g.,
+ * this.wnd.getFen) are not exported by GWT yet, so we use wrapper functions to
+ * provide call by name semantics.
+ */
+
+chessdog.Board.prototype.reset = function() {
+	if(assertChessdogReady(this)) {
+		return this.wnd.reset();
+	}
+}
+
+chessdog.Board.prototype.getFen = function() {
+	if(assertChessdogReady(this)) {
+		return this.wnd.getFen();
+	}
+}
+
+chessdog.Board.prototype.setFen = function(fen) {
+	var board = this;
+	onChessdogReady(this, function() {
+		board.wnd.setFen(fen);
+	});
+}
+
+chessdog.Board.prototype.getPgn = function() {
+	if(assertChessdogReady(this)){
+		return this.wnd.getPgn();
+	}
+}
+
+chessdog.Board.prototype.setPgn = function(pgn) {
+	var board = this;
+	onChessdogReady(this, function() {
+		board.wnd.setPgn(pgn);
+	});
+}
+
+chessdog.Board.prototype.onReady = function(fun) {
+	onChessdogReady(this, fun);
+}
+
+
+/*
+ * private functions
+ */
+
+var onChessdogReady = function(board, fun) {
+	if (board.wnd.chessdogReady) {
+		fun();
+	} else {
+		setTimeout(function() {	onChessdogReady(board, fun);}, 100);
+	}
+}
+
+var assertChessdogReady = function(board) {
+	if(board.wnd.chessdogReady) {
+		return true;
+	} else {
+		// TODO document / link better
+		alert("chessdog not ready yet. Have a look at board.onReady().");
+		return false;
+	}
+}
