ae46a57834f03902ba3e7dd5676a148a91aa383f
thunderklaus <christoph.dietze@gmail.com>
2011-11-02 22:40:29 +0100

    update GWT to 2.4.0 and GAE to 1.5.5

--- project/Build.scala
+++ project/Build.scala
@@ -19,6 +19,7 @@ object MyBuild extends Build {
       version := "1.0-SNAPSHOT",
       scalaVersion := "2.9.1",
       autoScalaLibrary := false,
+      gwtVersion := "2.4.0",
       gaeSdkPath := Option(System.getenv("APPENGINE_SDK_HOME")),
       unmanagedClasspath in Compile <++= (baseDirectory) map { b => Attributed.blankSeq((b ** "*.jar").get) },
       chessdogJsTempPath <<= (target) { (target) => target / "chessdog-temp" },
